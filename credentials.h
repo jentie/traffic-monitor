5a58c35916d7f17070814a3fdfd9acf5d745f0d0
jentie <jens@tiemaenner.de>
2020-01-27 10:53:35 +0100

    credential handling

--- credentials.h
+++ credentials.h
@@ -6,17 +6,8 @@
 
 
 // wifi credentials, upstream (existing WLAN) and downstream (new access point)
-//#define UP_WLAN_SSID "WLAN-NAME"
-//#define UP_WLAN_PASS "WLAN-PASSWORD"
-//#define DOWN_WLAN_SSID "WLAN-NAME"
-//#define DOWN_WLAN_PASS "WLAN-PASSWORD"
+#define UP_WLAN_SSID "WLAN-NAME"
+#define UP_WLAN_PASS "WLAN-PASSWORD"
+#define DOWN_WLAN_SSID "WLAN-NAME"
+#define DOWN_WLAN_PASS "WLAN-PASSWORD"
 
-//#define UP_WLAN_SSID "beiPfitze"
-//#define UP_WLAN_PASS "12345678"
-//#define DOWN_WLAN_SSID "dinge"
-//#define DOWN_WLAN_PASS "DingDong"
-
-#define UP_WLAN_SSID "OefitLabor1"
-#define UP_WLAN_PASS "HanD23!RoSe%/Ball!34WaLL"
-#define DOWN_WLAN_SSID "internet"
-#define DOWN_WLAN_PASS "oefitdemo"
