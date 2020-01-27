9b9f2c68443b5c41b91886c12d5ef43e3885c8bf
jentie <jens@tiemaenner.de>
2020-01-27 22:05:59 +0100

    include credentials.h once

---
+++ credentials.h
@@ -0,0 +1,12 @@
+//
+//   credentials.h - sensible und geheime Daten, wie Servernamen oder Passworte
+// 
+//   version 2020-01-10  
+//
+
+
+// wifi credentials, upstream (existing WLAN) and downstream (new access point)
+#define UP_WLAN_SSID "WLAN-NAME"
+#define UP_WLAN_PASS "WLAN-PASSWORD"
+#define DOWN_WLAN_SSID "WLAN-NAME"
+#define DOWN_WLAN_PASS "WLAN-PASSWORD"
