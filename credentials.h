808af33a95ab32f34dbe528ad804d25bb9b71d8f
jentie <jens@tiemaenner.de>
2020-01-27 10:48:44 +0100

    credential handling

--- credentials.h
+++ credentials.h
@@ -1,13 +1,22 @@
 //
-//   credentials.h - sensible und geheime Daten, wie Servernamen oder Passworte
+//   credentials.h - sensible and/or secret information, like network names, passwords, server names,...
 // 
-//   version 2020-01-10  
+//   version 2020-01-27  
 //
 
 
 // wifi credentials, upstream (existing WLAN) and downstream (new access point)
-const char* up_wlan_ssid = "WLAN-NAME";
-const char* up_wlan_pass = "WLAN-PASSWORD";
-const char* down_wlan_ssid = "WLAN-NAME";
-const char* down_wlan_pass = "WLAN-PASSWORD";
+//#define UP_WLAN_SSID "WLAN-NAME"
+//#define UP_WLAN_PASS "WLAN-PASSWORD"
+//#define DOWN_WLAN_SSID "WLAN-NAME"
+//#define DOWN_WLAN_PASS "WLAN-PASSWORD"
 
+//#define UP_WLAN_SSID "beiPfitze"
+//#define UP_WLAN_PASS "12345678"
+//#define DOWN_WLAN_SSID "dinge"
+//#define DOWN_WLAN_PASS "DingDong"
+
+#define UP_WLAN_SSID "OefitLabor1"
+#define UP_WLAN_PASS "HanD23!RoSe%/Ball!34WaLL"
+#define DOWN_WLAN_SSID "internet"
+#define DOWN_WLAN_PASS "oefitdemo"
